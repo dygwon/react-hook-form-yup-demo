69d7407d17ccd25809fefaea377c733d4389ff52
Daniel Gwon <t-danielgwon@microsoft.com>
2021-08-20 12:02:50 -0400

    create default values

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { Stack, IStackTokens, IStackStyles } from '@fluentui/react/lib/Stack';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import { useForm, FormProvider } from 'react-hook-form';
-import { PrimaryButton } from '@fluentui/react/lib/Button';
+import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
 import { ControlledTextField } from './components/controlledTextField';
 import { ControlledDropdown } from './components/controlledDropdown';
 import { ControlledCheckbox } from './components/controlledChoiceGroup';
 import { occupationDropdownOptions } from './models/occupationDropdownOptions';
-import { preferredCryptoOptions } from './models/preferredCryptoOptions';
 import { textFieldStyles } from './public/textFieldStyles';
 import { dropdownStyles } from './public/dropdownStyles';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -31,43 +30,106 @@ const stackStyles: Partial<IStackStyles> = {
  *   react-hook-form
  *   @hookform/resolvers yup
  *   yup
- * 
+ *
  * 1. useController
- * 2. control with FormProvider
- * 3. 
+ * 2. control and nesting with FormProvider
+ * 3. useFormContext provides same methods as useForm
+ * 4. schema-based validation
  */
 
-type Inputs = {
-  firstName: string,
-  lastName: string,
-  email: string,
-  website: string,
-  id: string,
-  occupation: string,
-  isCryptoHolder: boolean
-}
+type formInputs = {
+  firstName: string;
+  lastName?: string;
+  email: string;
+  website: string;
+  id: string;
+  occupation: string;
+  isCryptoHolder?: boolean;
+};
 
-const schema = yup.object().shape({
+const formSchema: SchemaOf<formInputs> = yup.object().shape({
   firstName: yup.string().required('Please provide a first name'),
   lastName: yup.string(),
-  email: yup.string().email(),
-  website: yup.string().required(),
-  id: yup.string().uuid().required(),
-  occupation: yup.string().required(),
-  isCryptoHolder: yup.boolean()
+  email: yup.string().email().required('Please provide an email'),
+  website: yup.string().url().required('Please provide a website'),
+  id: yup.string().uuid().required('Please provide an id'),
+  occupation: yup.string().required('Please select an occupation'),
+  isCryptoHolder: yup.boolean(),
 });
 
+const defaultValues: formInputs = {
+  firstName: 'Mat',
+  lastName: 'Fraser',
+  email: 'hwpo@crossfit.com',
+  website: 'https://www.hwpo.com',
+  id: '048dc551-234e-4d42-9492-c5bb7f9537f0',
+  occupation: 'Athlete',
+  isCryptoHolder: true,
+};
+
 export const App: React.FunctionComponent = () => {
-  const methods = useForm<Inputs>({ resolver: yupResolver(schema) });
-  const [result, setResult] = useState("");
+  const methods = useForm<formInputs>({
+    resolver: yupResolver(formSchema),
+    defaultValues: defaultValues,
+  });
+  const [result, setResult] = useState('');
   const onSubmit = (data: any) => setResult(JSON.stringify(data));
-  
+
+  const onResetClick = (): void => {
+    methods.reset();
+  };
+
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)} noValidate>
-        <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
-          <NestedInput />
-
+        <Stack
+          horizontalAlign="center"
+          verticalAlign="center"
+          verticalFill
+          styles={stackStyles}
+          tokens={stackTokens}
+        >
+          <ControlledTextField
+            name="firstName"
+            label="First Name"
+            isRequired
+            styles={textFieldStyles}
+          />
+          <ControlledTextField
+            name="lastName"
+            label="Last Name"
+            styles={textFieldStyles}
+          />
+          <ControlledTextField
+            name="email"
+            label="Email"
+            isRequired
+            styles={textFieldStyles}
+          />
+          <ControlledTextField
+            name="website"
+            label="Website"
+            isRequired
+            styles={textFieldStyles}
+          />
+          <ControlledTextField
+            name="id"
+            label="ID"
+            isRequired
+            styles={textFieldStyles}
+          />
+          <ControlledDropdown
+            name="occupation"
+            label="Occupation"
+            options={occupationDropdownOptions}
+            isRequired
+            styles={dropdownStyles}
+          />
+          <ControlledCheckbox
+            name="isCryptoHolder"
+            label="Do you hold any cryptocurrencies?"
+          />
+          <DefaultButton text="Reset" onClick={onResetClick} />
           <PrimaryButton type="submit" text="Submit" />
           <p>{result}</p>
         </Stack>
@@ -76,49 +138,6 @@ export const App: React.FunctionComponent = () => {
   );
 };
 
-const NestedInput: React.FunctionComponent = () => {
-  return (
-    <>
-      <ControlledTextField
-        name="firstName"
-        label="First Name"
-        isRequired
-        styles={textFieldStyles}
-      />
-      <ControlledTextField
-        name="lastName"
-        label="Last Name"
-        styles={textFieldStyles}
-      />
-      <ControlledTextField
-        name="email"
-        label="Email"
-        isRequired
-        styles={textFieldStyles}
-      />
-      <ControlledTextField
-        name="website"
-        label="Website"
-        isRequired
-        styles={textFieldStyles}
-      />
-      <ControlledTextField
-        name="id"
-        label="ID"
-        isRequired
-        styles={textFieldStyles}
-      />
-      <ControlledDropdown
-        name="occupation"
-        label="Occupation"
-        options={occupationDropdownOptions}
-        isRequired
-        styles={dropdownStyles}
-      />
-      <ControlledCheckbox
-        name="isCryptoHolder"
-        label="Do you hold any cryptocurrencies?"
-      />
-    </>
-  );
-}
+// const NestedInput: React.FunctionComponent = () => {
+//   return <></>;
+// };
